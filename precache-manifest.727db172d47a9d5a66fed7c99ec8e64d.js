0e015296799de542ae174ac38c3b420797c6b9d8
xprilion <xprilion@gmail.com>
2020-05-10 07:30:50 +0530

    Updates

---
+++ precache-manifest.727db172d47a9d5a66fed7c99ec8e64d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7bb072782307fce2d5fd420541ba9e1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e1213e28d888112a233",
+    "url": "./static/css/main.d854f1a2.chunk.css"
+  },
+  {
+    "revision": "1bb3d2f3e3d0d7b5045e",
+    "url": "./static/js/2.d963a571.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "./static/js/2.d963a571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1213e28d888112a233",
+    "url": "./static/js/main.0fb1da9b.chunk.js"
+  },
+  {
+    "revision": "961e7e55bd94730baf1e",
+    "url": "./static/js/runtime-main.78ca2889.js"
+  }
+]);
