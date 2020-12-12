3a3ceed795a9a3d862e26556305adc469fae975e
promugdha bhattacharya <mailtopromugdha@gmail.com>
2020-12-13 01:06:51 +0530

    stale

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import theme from './theme';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<ThemeProvider theme={theme}>
 		{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
 		<>
-		<CssBaseline />
-		<App />
+			<CssBaseline />
+			<App />
 		</>
 	</ThemeProvider>
 	,
