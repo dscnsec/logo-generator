64c00d0bb55647a06c24b7c7d7f12078e5634f58
promugdha bhattacharya <mailtopromugdha@gmail.com>
2020-12-13 01:08:57 +0530

    stale

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import theme from './theme';
-//import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<ThemeProvider theme={theme}>
