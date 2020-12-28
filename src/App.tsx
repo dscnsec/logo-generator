184600719033bca94e34343b69748ceb15535a50
Ayush <ayushmishra2854@gmail.com>
2020-12-29 00:02:34 +0530

    basic modal setUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import GDGEditor from './GDGEditor'
-import DSCEditor from './DSCEditor'
-import WTMEditor from './WTMEditor'
-import { AppBar, BottomNavigation, Box, Container, Link, Tabs, Tab, Typography } from '@material-ui/core';
+import GDGEditor from './GDGEditor';
+import DSCEditor from './DSCEditor';
+import WTMEditor from './WTMEditor';
+import { AppBar, BottomNavigation, Box, Container, Link, Tabs, Tab, Typography, Toolbar, Modal } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import GitHubButton from 'react-github-btn';
+import Button from '@material-ui/core/Button';
+import Login from './Login';
+import SignUp from './SignUp';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+
 
 interface Props{
 	props: any,
 	index:number,
 	mode:any
 }
+  
+  function getModalStyle() {
+	const top= 50;
+	const left=50;
+  
+	return {
+	  top: `${top}%`,
+	  left: `${left}%`,
+	  transform: `translate(-${top}%, -${left}%)`,
+	};
+  }
+  
 
 function TabPanel(props:any) {
 	const { children, value, index, ...other } = props;
@@ -53,25 +71,111 @@ const useStyles = makeStyles((theme) => ({
 		flexGrow: 1,
 		width: '100%',
 		backgroundColor: theme.palette.background.paper,
+		
 	},
+
+	menuButton: {
+		marginRight: theme.spacing(2),
+	  },
+	  title: {
+		flexGrow: 1,
+	  },
+	
 	stickToBottom: {
 		width: '100%',
 		position: 'fixed',
 		bottom: 0,
 	},
+	// stickToTop: {
+	// 	width: '100%',
+	// 	position: 'fixed',
+	// 	top:0,
+	// }
+	paper: {
+		position: 'absolute',
+		width: 400,
+		backgroundColor: theme.palette.background.paper,
+		// border: '2px solid #000',
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+	  },
+
+
 }));
 
 export default function ScrollableTabsButtonAuto() {
 	const classes = useStyles();
 	const [value, setValue] = React.useState(0);
+	const [modalStyle] = React.useState(getModalStyle);
+	const [openLogin, setOpenLogin] = React.useState(false);
+	const [openSignUp, setOpenSignUp] = React.useState(false);
+
 
 	const handleChange = (event:any, newValue:any) => {
 		setValue(newValue);
 	};
 
+	const handleOpen = () => {
+		setOpenLogin(true);
+	  };
+	
+	  const handleClose = () => {
+		setOpenLogin(false);
+	  };
+
+	  const LoginBody = (
+		<div style={modalStyle} className={classes.paper}>
+		  <Login />
+		</div>
+	  );
+
+	  const signUpBody = (
+		<div style={modalStyle} className={classes.paper}>
+		  <SignUp />
+	    </div>
+	  );
+	
+
 	return (
 		<div className={classes.root}>
+            {/* <AppBar position="sticky" color="default">
+                <Toolbar variant="dense">
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                     <MenuIcon />
+                      </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                         Logo-Generator
+                    </Typography>
+                     <Button color="inherit">Login</Button>
+					 <Button color="inherit">SignUp</Button>
+
+                </Toolbar>
+            </AppBar> */}
+
 			<AppBar position="static" color="default">
+			<Toolbar variant="dense">
+                    <Typography variant="h6" className={classes.title}>
+                         Logo-Generator
+                    </Typography>
+                    <Button color="inherit" onClick={handleOpen}>Login</Button>
+					   <Modal
+                        open={openLogin}
+                        onClose={handleClose}
+                        >
+                         {LoginBody}
+                       </Modal>
+
+					<Button color="inherit" onClick={() => setOpenSignUp(true)}>SignUp</Button>
+					<Modal
+                        open={openSignUp}
+                        onClose={() => setOpenSignUp(false)}
+                        >
+                         {signUpBody}
+                       </Modal>
+
+
+                </Toolbar>
+
 				<Tabs
 					value={value}
 					onChange={handleChange}
